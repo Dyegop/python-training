1ede0d57656e7550f91bfd44bf9c3ff512276e2f
dyegop <poncelas.professional@gmail.com>
2024-05-09 23:25:11 +0200

    refactor poetry notes

--- core/20_poetry.py
+++ core/20_poetry.py
@@ -27,35 +27,60 @@ build-backend = "poetry.core.masonry.api"
 
 ------ COMMANDS ------
 
-poetry self update <version>            - Upgrades Poetry to the last version. If <version> is
-                                        given, it upgrades/downgrades Poetry to that version.
-poetry new <folder_name>  	 			- Create new project.
-poetry add <package_name> 	 			- Add an external package to your project.
-poetry add <package_name> -D/--dev 		- Add an external package specifying that it is a dev
-                                        dependency.
-poetry add `cat requirements.txt`		- Add dependecies to a project from requirements txt file.
-poetry --version		  	 			- Print installed version of poetry.
-poetry env list			     			- List installed envs in the current directory.
-poetry install		         			- Check for dependencies, resolves and install them. A
-                                        pyproject.toml is required.
-poetry lock                             - Reads the pyproject.toml file from the current
-                                        directory, processes it, and locks the dependencies in
-                                        the poetry.lock file.
-poetry update							-> Update all dependencies
-poetry update <dependency_name>			-> Update a given dependecy or dependecies.
-poetry export --output requirements.txt -> Export poetry dependecies to a requirements file.
-poetry env use "path/to/python.exe"     -> Use env for the python version in the given path the
-                                        path must be added with quotation marks.
+poetry self update <version>
+Upgrades Poetry to the last version.
+If <version> is given, it upgrades/downgrades Poetry to that version.
+
+poetry new <folder_name>
+Create new project.
+
+poetry add <package_name>
+Add an external package to your project.
+
+poetry add <package_name> -D/--dev
+Add an external package specifying that it is a dev dependency.
+
+poetry add `cat requirements.txt`
+Add dependecies to a project from requirements txt file.
+
+poetry --version
+Print installed version of poetry.
+
+poetry env list
+List installed envs in the current directory.
+
+poetry install
+Check for dependencies, resolves and install them. A pyproject.toml is required.
+
+poetry install --compile --no-root --with dev --all-extras
+Installs all dependencies, including development dependencies and all extras, but excludes the
+project itself as a dependency, and compiles the dependencies if possible.
+
+poetry lock
+Reads the pyproject.toml file from the current directory, processes it, and locks the dependencies
+in the poetry.lock file.
+
+poetry update
+Update all dependencies
+
+poetry update <dependency_name>
+Update a given dependecy or dependecies.
+
+poetry export --output requirements.txt
+Export poetry dependecies to a requirements file.
+
+Use env for the python version in the given path the path must be added with quotation marks.
+poetry env use "path/to/python.exe"
 
 
 
 
 ------ CONFIGURATION ------
 
-The pyproject.toml file is divided in sections called tables. 
+The pyproject.toml file is divided in sections called tables.
 They contain instructions that tools like Poetry recognize and use for dependency management or
 build routines.
-If a table name is tool-specific, it must be prefixed with tool keyword. 
+If a table name is tool-specific, it must be prefixed with tool keyword.
 By using such a subtable, you can add instructions for different tools in your project.
 
 Subtables:
